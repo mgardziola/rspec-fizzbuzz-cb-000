46a4de7677062aebdc9b5d10eecb66987ee66439
mariah <mariah@gardziola.com>
2019-02-15 17:03:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,15 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
+  if int % 3
+    "Fizz"
+  end
+  
   if int % 3 && int % 5
   "FizzBuzz"
   end
       
-  if int % 3
-    "Fizz"
-  end
+
     
   if int % 5 == 0
     "Buzz"
