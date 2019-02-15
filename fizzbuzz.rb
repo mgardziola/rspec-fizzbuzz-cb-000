10e36b87b63a91b8189c9d77892c0e39ecc55162
mariah <mariah@gardziola.com>
2019-02-15 16:38:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3
+    "Fizz"
+    
+  elsif int % 5 
+    "Buzz"
+
+  end
+
+
+
+end
